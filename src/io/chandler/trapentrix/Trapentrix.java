0ea3bbab5bcde26acc707a1f8503e6df69c1a6f5
Chandler Griscom <cjgriscom@gmail.com>
2018-11-23 13:00:29 -0500

    Initial commit: puzzle definition

---
+++ src/io/chandler/trapentrix/Trapentrix.java
@@ -0,0 +1,87 @@
+package io.chandler.trapentrix;
+
+import static io.chandler.trapentrix.Trapentrix.Piece.*;
+
+public class Trapentrix {
+	
+	public static Orbit grip1_close_low  = new Orbit(F1D, F1g, B1C);
+	public static Orbit grip1_close_high = new Orbit(F1C, B1g, B1D);
+	public static Orbit grip1_far_low    = new Orbit(F2D, F1m, B_U);
+	public static Orbit grip1_far_high   = new Orbit(F_U, B1m, B2D);
+	
+	public static Orbit grip2_close_low  = new Orbit(F2D, F2g, B2C);
+	public static Orbit grip2_close_high = new Orbit(F2C, B2g, B2D);
+	public static Orbit grip2_far_low    = new Orbit(F1D, F2m, B_U);
+	public static Orbit grip2_far_high   = new Orbit(F_U, B2m, B1D);
+
+	public static Move grip1Down = new Move(4)
+			.add(grip1_close_low, 1)
+			.add(grip1_close_high, 1)
+			.add(grip1_far_low, 1)
+			.add(grip1_far_high, 1);
+	public static Move grip1Up = new Move(4)
+			.add(grip1_close_low, -1)
+			.add(grip1_close_high, -1)
+			.add(grip1_far_low, -1)
+			.add(grip1_far_high, -1);
+	public static Move grip2Down = new Move(4)
+			.add(grip2_close_low, 1)
+			.add(grip2_close_high, 1)
+			.add(grip2_far_low, 1)
+			.add(grip2_far_high, 1);
+	public static Move grip2Up = new Move(4)
+			.add(grip2_close_low, -1)
+			.add(grip2_close_high, -1)
+			.add(grip2_far_low, -1)
+			.add(grip2_far_high, -1);
+	
+	public Piece[] reference = Piece.values().clone();
+	public Piece[] state = Piece.values().clone();
+	
+	public static class Move {
+		public final Orbit[] orbits;
+		public final int[] directions;
+		private int entered = 0;
+		public Move(int num) {
+			this.orbits = new Orbit[num];
+			this.directions = new int[num];
+		}
+		public Move add(Orbit o, int dir) {
+			orbits[entered] = o;
+			directions[entered++] = dir;
+			return this;
+		}
+	}
+	
+	public static class Orbit {
+		public final Piece[] buffer;
+		public final Piece[] members;
+		public Orbit(Piece... members) {this.members = members;this.buffer = new Piece[members.length];}
+	}
+	
+	public static enum Piece {
+		F_U('r'),
+		F1D('r'),
+		F1C('r'),
+		F2D('r'),
+		F2C('r'),
+		F1m('b'),
+		F1g('b'),
+		F2m('g'),
+		F2g('g'),
+		B1m('b'),
+		B1g('b'),
+		B2m('g'),
+		B2g('g'),
+		B_U('o'),
+		B1D('o'),
+		B1C('o'),
+		B2D('o'),
+		B2C('o'),
+		;
+		final char color;
+		Piece(char color) {
+			this.color = color;
+		}
+	}
+}
