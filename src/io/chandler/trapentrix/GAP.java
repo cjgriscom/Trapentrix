1cb08db407ea388b979c43d60415004ee8665cfb
Chandler Griscom <cjgriscom@gmail.com>
2024-08-29 16:28:29 -0400

    GAP improvements

--- src/io/chandler/trapentrix/GAP.java
+++ src/io/chandler/trapentrix/GAP.java
@@ -1,20 +1,17 @@
 package io.chandler.trapentrix;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
 public class GAP {
-	// M6 / Eliac: [ (1,2,3,4)(5,6,7,8), (1,5)(2,9)(4,10) ]
+    // M6 / Eliac: [ (1,2,3,4)(5,6,7,8), (1,5)(2,9)(4,10) ]
     private static Map<State, Boolean> stateMap = new HashMap<>();
     private static int[] elements;
-
-    private static final int MAX_STACK_SIZE = 100000;
+    private static List<int[][]> parsedOperations;
 
     public static class State {
         int[] state;
@@ -33,21 +30,21 @@ public class GAP {
     }
 
     public static void main(String[] args) {
-        //String cycleNotation = "[(1,2,3,4)(5,6,7,8),(1,5)(2,9)(4,10))]";
-        String cycleNotation = "[(1,2,3,4)(5,6,7,8),(1,5)(2,9)(4,10),(7,3)(11,4)(2,12))]";
-		int nElements = 0;
-		for (String s : cycleNotation.split("\\(|\\)|,|\\[|\\]")) {
-			if (!s.trim().isEmpty()) nElements = Math.max(nElements, Integer.parseInt(s.trim()));
-		}
+        String cycleNotation = "[(1,2,3,4)(5,6,7,8),(1,5)(2,9)(4,10))]";
+        int nElements = 0;
+        for (String s : cycleNotation.split("\\(|\\)|,|\\[|\\]")) {
+            if (!s.trim().isEmpty()) nElements = Math.max(nElements, Integer.parseInt(s.trim()));
+        }
         System.out.println("nElements: " + nElements);
         long nPermutations = 1;
-		for (int i = 1; i <= nElements; i++) {
-			nPermutations *= i;
-		}
-		System.out.println("Total unique permutations: " + nPermutations);
+        for (int i = 1; i <= nElements; i++) {
+            nPermutations *= i;
+        }
+        System.out.println("Total unique permutations: " + nPermutations);
 
         elements = initializeElements(nElements);
-        int iterations = exploreStates(cycleNotation, elements);
+        parsedOperations = parseOperations(cycleNotation);
+        int iterations = exploreStates(elements);
         System.out.println("Total group permutations: " + stateMap.size());
 
         System.out.println("Subset: " + ((double)nPermutations / stateMap.size()));
@@ -62,15 +59,13 @@ public class GAP {
         return result;
     }
 
-    private static int exploreStates(String groupNotation, int[] initialState) {
+    private static int exploreStates(int[] initialState) {
         stateMap.put(new State(initialState), false);
-
-        List<String> operations = parseOperations(groupNotation);
 
         int iteration = 0;
         while (true) {
+            System.out.println("Depth: " + iteration + " - " + stateMap.size());
             iteration++;
-            System.out.println("Iteration: " + iteration + " - " + stateMap.size());
             HashMap<State, Boolean> stateMapCopy = new HashMap<>(stateMap);
 
             for (Entry<State, Boolean> entry : stateMap.entrySet()) {
@@ -81,7 +76,7 @@ public class GAP {
                 int[] currentState = state.state;
 
                 boolean added = false;
-                for (String operation : operations) {
+                for (int[][] operation : parsedOperations) {
                     int[] newState = applyOperation(currentState, operation);
 
                     Boolean exhausted = stateMapCopy.get(new State(newState));
@@ -98,34 +93,49 @@ public class GAP {
                 return iteration;
             }
             stateMap = stateMapCopy;
-
         }
-
     }
 
-    private static List<String> parseOperations(String groupNotation) {
-        List<String> operations = new ArrayList<>();
+    private static List<int[][]> parseOperations(String groupNotation) {
+        List<int[][]> operations = new ArrayList<>();
         String[] parts = groupNotation.substring(1, groupNotation.length() - 1).split("\\),\\(");
         for (String part : parts) {
-            operations.add("(" + part + ")");
+            String[] cycles = part.split("\\)\\(");
+            int[][] operation = new int[cycles.length][];
+            for (int i = 0; i < cycles.length; i++) {
+                String[] elements = cycles[i].replaceAll("[()]", "").split(",");
+                operation[i] = Arrays.stream(elements).mapToInt(Integer::parseInt).toArray();
+            }
+            operations.add(operation);
+            operations.add(reverseOperation(operation));
         }
         return operations;
     }
 
-    private static int[] applyOperation(int[] state, String operation) {
+    private static int[][] reverseOperation(int[][] operation) {
+        int[][] reversedOps = new int[operation.length][];
+        for (int o = 0; o < operation.length; o++) {
+            int[] cycle = operation[o];
+            int[] reversed = new int[cycle.length];
+            for (int i = 0; i < cycle.length; i++) {
+                reversed[i] = cycle[cycle.length - i - 1];
+            }
+            reversedOps[o] = reversed;
+        }
+        return reversedOps;
+    }
+
+    private static int[] applyOperation(int[] state, int[][] operation) {
         int[] newState = Arrays.copyOf(state, state.length);
-        String[] cycles = operation.split("\\)\\(");
-        for (String cycle : cycles) {
-            cycle = cycle.replaceAll("[()]", "");
-            String[] elements = cycle.split(",");
-            if (elements.length > 1) {
-                int first = Integer.parseInt(elements[0]);
-                for (int i = 0; i < elements.length - 1; i++) {
-                    int current = Integer.parseInt(elements[i]);
-                    int next = Integer.parseInt(elements[i + 1]);
+        for (int[] cycle : operation) {
+            if (cycle.length > 1) {
+                int first = cycle[0];
+                for (int i = 0; i < cycle.length - 1; i++) {
+                    int current = cycle[i];
+                    int next = cycle[i + 1];
                     newState[current - 1] = state[next - 1];
                 }
-                newState[Integer.parseInt(elements[elements.length - 1]) - 1] = state[first - 1];
+                newState[cycle[cycle.length - 1] - 1] = state[first - 1];
             }
         }
         return newState;
